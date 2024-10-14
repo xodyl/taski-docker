9d56408da668951021cd1bd9038a7eb2ccfec22d
Boris Potapov <borisportretov@gmail.com>
2024-10-14 18:00:14 +0100

    fix flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,5 @@
-from pathlib import Path
 import os
-
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'practicumtaski.zapto.org', '158.160.77.252'] # айпи и домен сервака
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'practicumtaski.zapto.org', '158.160.77.252']
 
 # Application definition
 
